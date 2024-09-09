3bd64508b41c07c83d3adc1ad838b8dbb31de2db
Kauan <kauansantosdesouza45@gmail.com>
2024-09-09 19:36:21 -0300

    Realizado as alterações importante e colocado o gitignore

--- bot/env_email.py
+++ bot/env_email.py
@@ -1,37 +1,41 @@
 import win32com.client as win32
 import pandas as pd
 
-teste = pd.read_excel("xlsx/CHECKLIST_SALAS_2024 (Salvo automaticamente).xlsx")
+teste = pd.read_excel("xlsx/emprestimo.xlsx")
 
-if 'STATUS' in teste.columns:
-    test = teste[teste['STATUS'] == "CONCLUÍDA"]
+if 'Status' in teste.columns:
+    test = teste[teste['Status'] == "Não Entregue"]
+    if len(test) >= 1:
+        mensagem = [f""" <p>
+            <span>
+            <strong>Equipamento:</strong> {row['Equipamento']}
+            </span>, 
+            <span>
+            <strong>status:</strong> {row['Status']}</span>, 
+            <span>
+            <strong>Sala:</strong> {row['Sala']},
+            </span>
+            <span><strong>Data:</strong> {row['Data']}</span>
+            </p>\n""" for index, row in test.iterrows()]
+        mensagem_email = "\n".join(mensagem)
+
+        print(mensagem_email)
+
+        outlook = win32.Dispatch("outlook.application")
+        email = outlook.CreateItem(0)
+        email.To = "kauansantosdesouza45@gmail.com"
+        email.Subject = "Relatório de Emprestimo"
+        email.HTMLBody = f"""
+            <h2>Relátorio de Salas</h2>
+            {mensagem_email}
+        """
+
+        email.Send()
+        print("Email Enviado")
+    else:
+        print("Não foi encontrado nenhum equipamento não entregue")
 else:
     print("A coluna 'STATUS' não foi encontrada.")
 
+    
 
-mensagem = [f""" <p>
-            <span>
-            <strong>Nome do espaço:</strong> {row['NOME DO ESPAÇO']}
-            </span>, 
-            <span>
-            <strong>status:</strong> {row['STATUS']}</span>, 
-            <span>
-            <strong>Capacidade:</strong> {row['CAPACIDADE']},
-            </span>, 
-            <span><strong>Modelo do Computador:</strong> {row['COMPUTADO MODELO']}</span>
-            </p>\n""" for index, row in test.iterrows()]
-mensagem_email = "\n".join(mensagem)
-
-print(mensagem_email)
-
-outlook = win32.Dispatch("outlook.application")
-email = outlook.CreateItem(0)
-email.To = "kauansantosdesouza45@gmail.com"
-email.Subject = "Relatório de Emprestimo"
-email.HTMLBody = f"""
-    <h2>Relátorio de Salas</h2>
-    {mensagem_email}
-"""
-
-email.Send()
-print("Email Enviado")
