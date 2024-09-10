283b4acfd38dfc4ef9a93320f25fcd182cc459dd
Kauan <kauansantosdesouza45@gmail.com>
2024-09-10 19:10:56 -0300

    Adicionado comando para exluir arquivos antigos de excell e fazer download de arquivos novos de excell no teams.

--- teste.py
+++ teste.py
@@ -1,9 +1,52 @@
 import pyautogui
-import subprocess
+from time import sleep
 
+
+def soneca():
+    return sleep(1)
+
+# Excluir arquivos de excell antigos 
+pyautogui.doubleClick(x=30, y=10)
+soneca()
+pyautogui.doubleClick(x=450, y=310)
+pyautogui.keyDown("ctrl")
+soneca()
+pyautogui.press("a")
+pyautogui.keyUp("ctrl")
+soneca()
+pyautogui.keyDown("shift")
+soneca
+pyautogui.press("del")
+pyautogui.keyUp("shift")
+soneca()
+pyautogui.click(x=690, y=520)
+soneca()
+pyautogui.keyDown('alt')
+soneca()
+pyautogui.press("F4")
+pyautogui.keyUp("alt")
+soneca()
+# Excluir arquivos de excell antigos 
+# ////////////////////////////////////////////////
+# abrir e fazer download do arquivo
 pyautogui.press("win")
+soneca()
 pyautogui.write("Teams")
-subprocess.run( shell=True)
-pyautogui.click(x=500, y=50)
-
+soneca()
 pyautogui.press("enter")
+sleep(8)
+pyautogui.click(x=830, y=65)
+soneca()
+pyautogui.click(x=830, y=200)
+pyautogui.scroll(-300)
+soneca()
+pyautogui.rightClick(x=997, y=487)
+soneca()
+pyautogui.click(x=1025, y=580)
+soneca()
+pyautogui.keyDown('alt')
+soneca()
+pyautogui.press("F4")
+pyautogui.keyUp("alt")
+# abrir e fazer download do arquivo
+# ////////////////////////////////////////////////
