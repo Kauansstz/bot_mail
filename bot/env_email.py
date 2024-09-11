ff1684962f9610724f25a8be57228bec3eb56322
Kauan <kauansantosdesouza45@gmail.com>
2024-09-11 16:01:30 -0300

    Colocado o pyautogui no script env_mail

--- bot/env_email.py
+++ bot/env_email.py
@@ -1,11 +1,63 @@
 import win32com.client as win32
 import pandas as pd
+import pyautogui
+from time import sleep
 
-teste = pd.read_excel("xlsx/emprestimo.xlsx")
 
-if 'Status' in teste.columns:
-    test = teste[teste['Status'] == "Não Entregue"]
-    if len(test) >= 1:
+def soneca():
+    return sleep(1)
+
+# Excluir arquivos de excell antigos 
+pyautogui.doubleClick(x=30, y=10)
+soneca()
+pyautogui.doubleClick(x=450, y=310)
+pyautogui.keyDown("ctrl")
+soneca()
+pyautogui.press("a")
+pyautogui.keyUp("ctrl")
+soneca()
+pyautogui.keyDown("shift")
+soneca
+pyautogui.press("del")
+pyautogui.keyUp("shift")
+soneca()
+pyautogui.click(x=690, y=520)
+soneca()
+pyautogui.keyDown('alt')
+soneca()
+pyautogui.press("F4")
+pyautogui.keyUp("alt")
+soneca()
+# Excluir arquivos de excell antigos 
+# ////////////////////////////////////////////////
+# abrir e fazer download do arquivo
+pyautogui.press("win")
+soneca()
+pyautogui.write("Teams")
+soneca()
+pyautogui.press("enter")
+sleep(8)
+pyautogui.click(x=830, y=65)
+soneca()
+pyautogui.click(x=830, y=200)
+pyautogui.scroll(-300)
+soneca()
+pyautogui.rightClick(x=997, y=487)
+soneca()
+pyautogui.click(x=1025, y=580)
+soneca()
+pyautogui.keyDown('alt')
+soneca()
+pyautogui.press("F4")
+pyautogui.keyUp("alt")
+# abrir e fazer download do arquivo
+# ////////////////////////////////////////////////
+ 
+
+dock = pd.read_excel("xlsx/emprestimo.xlsx")
+if 'Status' in dock.columns:
+    dock_read = dock[dock['Status'] == "Não Entregue"]
+    if len(dock_read) >= 1:
         mensagem = [f""" <p>
             <span>
             <strong>Equipamento:</strong> {row['Equipamento']}
@@ -16,7 +68,7 @@ if 'Status' in teste.columns:
             <strong>Sala:</strong> {row['Sala']},
             </span>
             <span><strong>Data:</strong> {row['Data']}</span>
-            </p>\n""" for index, row in test.iterrows()]
+            </p>\n""" for index, row in dock_read.iterrows()]
         mensagem_email = "\n".join(mensagem)
 
         print(mensagem_email)
@@ -33,7 +85,7 @@ if 'Status' in teste.columns:
         email.Send()
         print("Email Enviado")
     else:
-        print("Não foi encontrado nenhum equipamento não entregue")
+        print("Não foi encontrado nenhum equipamento 'não entregue'.")
 else:
     print("A coluna 'STATUS' não foi encontrada.")
 
